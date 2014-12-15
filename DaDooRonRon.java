93e060b04f2b7196bfcfa88e74f8844dbc5e6415
veev <vivyee@bu.edu>
2014-12-14 22:46:02 -0500

    add filez

---
+++ DaDooRonRon.java
@@ -0,0 +1,78 @@
+public class DaDooRonRon {
+    public static void writeD() {
+        System.out.println("-----");
+        System.out.println("|    \\");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("|    /");
+        System.out.println("-----");
+    }
+    public static void writeA() {
+        System.out.println("+-----+");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("+-----+");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("|     |");
+    }
+    public static void writeO() {
+        System.out.println("+-----+");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("+-----+");
+    }
+    public static void writeR() {
+        System.out.println("+----");
+        System.out.println("|    \\");
+        System.out.println("|     |");
+        System.out.println("|____/");
+        System.out.println("|  \\");
+        System.out.println("|   \\");
+        System.out.println("|    \\");
+    }
+    public static void writeN() {
+        System.out.println("|     |");
+        System.out.println("|\\    |");
+        System.out.println("| \\   |");
+        System.out.println("|  \\  |");
+        System.out.println("|   \\ |");
+        System.out.println("|    \\|");
+        System.out.println("|     |");
+    }
+    public static void bigSpace() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        writeD();
+        System.out.println();
+        writeA();
+        bigSpace();
+        writeD();
+        System.out.println();
+        writeO();
+        System.out.println();
+        writeO();
+        bigSpace();
+        writeR();
+        System.out.println();
+        writeO();
+        System.out.println();
+        writeN();
+        bigSpace();
+        writeR();
+        System.out.println();
+        writeO();
+        System.out.println();
+        writeN();
+        System.out.println();
+    }
+} 
